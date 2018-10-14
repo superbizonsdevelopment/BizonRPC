1935e1aa3578f769efd1ec7ed4818889cea0c366
Anthony <antoni.p.bednarski@gmail.com>
2018-10-14 14:12:45 +0200

    update

--- application.go
+++ application.go
@@ -13,26 +13,3 @@ type Application struct {
 func New(applicationID string) Application {
 	return &Application{ID: applicationID, System: GetSystem(), TempPath: GetTempPath(), Connected: false}
 }
-
-/*
-func (app *Application) Connect() error {
-	conn, err := net.Dial(app.System, app.TempPath+"discord-rpc")
-
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-func (app *Application) SetRichPresence() {
-
-}
-
-func (app *Application) IsConnect() {
-
-}
-
-func (app *Application) Disconnect() {
-
-}
-*/
